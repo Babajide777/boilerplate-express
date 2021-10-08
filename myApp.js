a56ceca00dfc53e8edb226d36c328ba509105a82
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2021-10-08 11:18:51 -0500

    json route

--- myApp.js
+++ myApp.js
@@ -1,10 +1,23 @@
 var express = require("express");
 var app = express();
+const path = require("path");
+let public = path.join(__dirname, "/public");
+require("dotenv").config();
+
+const MESSAGE_STYLE = process.env.MESSAGE_STYLE;
+
+app.use(express.static(public));
 
 console.log("Hello World");
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+
+app.get("/json", (req, res) => {
+  MESSAGE_STYLE === "uppercase"
+    ? res.json({ message: "Hello json".toUpperCase() })
+    : res.json({ message: "Hello json" });
 });
 
 module.exports = app;
